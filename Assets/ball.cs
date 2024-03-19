292fc8fb307ea239a92ebccde550c59bf3403f6a
Kazumi Sugawara <kazumi@mike-neko.biz>
2024-03-19 19:15:45 +0900

    add sound

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -12,34 +12,53 @@ public class ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 10);
+        GameInitialize();
+    }
+
+    void GameInitialize(){
+        scoreA = 0;
+        scoreB = 0;
+        clear = false;
         GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().enabled = false;
         GameObject.Find("Message").GetComponent<UnityEngine.UI.Text>().enabled = false;
+        GameObject.Find("ScoreA").GetComponent<UnityEngine.UI.Text>().text = scoreA.ToString();
+        GameObject.Find("ScoreB").GetComponent<UnityEngine.UI.Text>().text = scoreB.ToString();
+        GetComponent<Renderer>().enabled = true;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(clear){
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().enabled = true;
-            GameObject.Find("Message").GetComponent<UnityEngine.UI.Text>().enabled = true;
-            if(Input.GetKey(KeyCode.Return))
-            {
-                scoreA = 0;
-                scoreB = 0;
-                clear = false;
-                GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().enabled = false;
-                GameObject.Find("Message").GetComponent<UnityEngine.UI.Text>().enabled = false;
-                GameObject.Find("ScoreA").GetComponent<UnityEngine.UI.Text>().text = scoreA.ToString();
-                GameObject.Find("ScoreB").GetComponent<UnityEngine.UI.Text>().text = scoreB.ToString();
-                this.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 10);
-            }
+        // ゲームクリア画面でエンターキーを押したら再プレイ
+        if(clear && Input.GetKey(KeyCode.Return)){
+            GameInitialize();
         }
+    }
+
+    void GameClear(){
+        clear = true;
+        GameObject.Find("SoundWin").GetComponent<AudioSource>().Play();
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().enabled = true;
+        GameObject.Find("Message").GetComponent<UnityEngine.UI.Text>().enabled = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // ボールがラケットに当たったら音を鳴らす
+        if(other.gameObject.name == "PlayerA" || other.gameObject.name == "PlayerB")
+        {
+            GameObject.Find("SoundBall").GetComponent<AudioSource>().Play();
+        }
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        // ボールがゴールに入ったら初期位置に戻す
+        GameObject.Find("SoundBeep").GetComponent<AudioSource>().Play();
         StartCoroutine(BallInit(other.name));
     }
 
@@ -47,28 +66,30 @@ public class ball : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         transform.position = new Vector3(0, 4, 0);
-       if(name == "Square (2)")
+       if(name == "GoalLeft")
         {
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 10);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(10, 10);
             scoreB += 10;
             GameObject.Find("ScoreB").GetComponent<UnityEngine.UI.Text>().text = scoreB.ToString();
         }
-        else if(name == "Square (3)")
+        else if(name == "GoalRight")
         {
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 10);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 10);
             scoreA += 10;
             GameObject.Find("ScoreA").GetComponent<UnityEngine.UI.Text>().text = scoreA.ToString();
         }
+
         // 先に100点を取った方が勝ち
         if(scoreA >= 100)
         {
             GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().text = "Player A Win!";
-            clear = true;
+            GameClear();
         }
         else if(scoreB >= 100)
         {
             GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().text = "Player B Win!";
-            clear = true;
+            GameClear();
+
         }
     }
 
