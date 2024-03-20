3686a6903da6b132ccfb7f8a531c1e0d95ce997b
Kazumi Sugawara <kazumi@mike-neko.biz>
2024-03-20 21:42:31 +0900

    add player name, remove unnecessary code

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -7,85 +7,16 @@ public class ball : MonoBehaviour
 
     private GameManager manager;
 
-
-
-
-
-    //private int scoreA = 0;
-    //private int scoreB = 0;
-    //public GameState state = GameState.Opening;
-
-    //private float speed = 0.3f;
-    //public float difficulty = 1.0f;
-
-    // Start is called before the first frame update
     void Start()
     {
         manager = GameObject.Find("Frame").GetComponent<GameManager>();
     }
 
-    /*public void Opening(){
-        state = GameState.Opening;
-        GetComponent<Renderer>().enabled = false;
-        GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().enabled = false;
-        GameObject.Find("Message").GetComponent<UnityEngine.UI.Text>().enabled = false;
-        GameObject.Find("BtnMain").GetComponent<UnityEngine.UI.Button>().enabled = true;
-    }
-
-    public void GameInitialize(){ 
-        scoreA = 0;
-        scoreB = 0;
-        speed = 0.3f * difficulty;
-        state = GameState.Game;
-        GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().enabled = false;
-        GameObject.Find("Message").GetComponent<UnityEngine.UI.Text>().enabled = false;
-        GameObject.Find("BtnMain").GetComponent<UnityEngine.UI.Button>().enabled = false;
-        GameObject.Find("ScoreA").GetComponent<UnityEngine.UI.Text>().text = scoreA.ToString();
-        GameObject.Find("ScoreB").GetComponent<UnityEngine.UI.Text>().text = scoreB.ToString();
-        GetComponent<Renderer>().enabled = true;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(-10*speed, 10*speed);
-    }*/
-
     // Update is called once per frame
     void Update()
     {
-        /*
-        // ゲームクリア画面でエンターキーを押したら再プレイ
-        if(state == GameState.Clear && Input.GetKey(KeyCode.Return)){
-            GameInitialize();
-        }
-        // オープニング画面での難易度選択
-        if(state == GameState.Opening){
-            if(Input.GetKeyDown(KeyCode.UpArrow) && difficulty > 1.0f){
-                difficulty -= 1.0f;
-                GameObject.Find("Cursor").transform.position += new Vector3(0,1,0);
-            }
-            if(Input.GetKeyDown(KeyCode.DownArrow) && difficulty < 3.0f){
-                difficulty += 1.0f;
-                GameObject.Find("Cursor").transform.position += new Vector3(0,-1,0);
-            } 
-            if(Input.GetKeyDown(KeyCode.Return)){
-                GameObject.Find("Opening").SetActive(false);
-                GameInitialize();
-            }
-        }
 
-        // ESCで終了
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            Application.Quit();
-        }
-        */
     }
-
-    /*void GameClear(){
-        state = GameState.Clear;
-        GameObject.Find("SoundWin").GetComponent<AudioSource>().Play();
-        GetComponent<Renderer>().enabled = false;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().enabled = true;
-        GameObject.Find("Message").GetComponent<UnityEngine.UI.Text>().enabled = true;
-        GameObject.Find("BtnMain").GetComponent<UnityEngine.UI.Button>().enabled = true;
-    }*/
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -94,7 +25,6 @@ public class ball : MonoBehaviour
         {
             GameObject.Find("SoundBall").GetComponent<AudioSource>().Play();
         }
-
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -103,36 +33,5 @@ public class ball : MonoBehaviour
         GameObject.Find("SoundBeep").GetComponent<AudioSource>().Play();
         manager.Goal(other.name);
     }
-
-/*    IEnumerator BallInit(string name)
-    {
-        yield return new WaitForSeconds(2);
-        transform.position = new Vector3(0, 4, 0);
-       if(name == "GoalLeft")
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(10*speed, 10*speed);
-            scoreB += 10;
-            GameObject.Find("ScoreB").GetComponent<UnityEngine.UI.Text>().text = scoreB.ToString();
-        }
-        else if(name == "GoalRight")
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-10*speed, 10*speed);
-            scoreA += 10;
-            GameObject.Find("ScoreA").GetComponent<UnityEngine.UI.Text>().text = scoreA.ToString();
-        }
-
-        // 先に100点を取った方が勝ち
-        if(scoreA >= 100)
-        {
-            GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().text = "Player A Win!";
-            GameClear();
-        }
-        else if(scoreB >= 100)
-        {
-            GameObject.Find("Winner").GetComponent<UnityEngine.UI.Text>().text = "Player B Win!";
-            GameClear();
-
-        }
-    }*/
 
 }
--- Assets/button.cs
+++ Assets/button.cs
@@ -21,56 +21,6 @@ public class button : MonoBehaviour
     }
 
     public void Click(){
-
         manager.InputUIButton(this.name);
-        
-        /*GameObject barA = GameObject.Find("PlayerA");
-        GameObject barB = GameObject.Find("PlayerB");
-        ball cball = GameObject.Find("Ball").GetComponent<ball>();
-        GameState state = cball.state;
-
-        // ボタンが押された時にバーを移動させる
-        if(state == GameState.Game){
-            switch(this.name){
-                case "BtnUpA":
-                    if(barA.transform.position.y < 4.5f){
-                        barA.transform.position += new Vector3(0, 1.0f, 0);
-                    }
-                    break;
-                case "BtnDownA":
-                    if(barA.transform.position.y > -4.5f){
-                        barA.transform.position -= new Vector3(0, 1.0f, 0);
-                    }
-                    break;
-                case "BtnUpB":
-                    if(barB.transform.position.y < 4.5f){
-                        barB.transform.position += new Vector3(0, 1.0f, 0);
-                    }
-                    break;
-                case "BtnDownB":
-                    if(barB.transform.position.y > -4.5f){
-                        barB.transform.position -= new Vector3(0, 1.0f, 0);
-                    }
-                    break;
-            }             
-        }
-        if(state == GameState.Clear && this.name == "BtnMain"){
-            GameObject.Find("Ball").GetComponent<ball>().GameInitialize();
-        }
-        if(state == GameState.Opening){
-            if((this.name=="BtnUpA" || this.name=="BtnUpB") && cball.difficulty > 1.0f){
-                cball.difficulty -= 1.0f;
-                GameObject.Find("Cursor").transform.position += new Vector3(0,1,0);
-            }
-            if((this.name=="BtnDownA" || this.name=="BtnDownB") && cball.difficulty < 3.0f){
-                cball.difficulty += 1.0f;
-                GameObject.Find("Cursor").transform.position += new Vector3(0,-1,0);
-            }
-            if(this.name=="BtnMain"){
-                GameObject.Find("Opening").SetActive(false);
-                GameObject.Find("Ball").GetComponent<ball>().GameInitialize();
-            }
-        } */     
-
     }
 }
